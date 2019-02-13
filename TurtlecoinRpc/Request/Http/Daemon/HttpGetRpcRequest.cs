b1835905712125d86b7e54d23cfa014d731b25bd
Yong Jian Ming <jianming1993@gmail.com>
2019-02-13 10:26:04 +0800

    Remove index

--- TurtlecoinRpc/Request/Http/Daemon/HttpGetRpcRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TurtlecoinRpc.Request.Http.Daemon
-{
-    internal class HttpGetRpcRequest<TResponse> : Http.HttpGetRpcRequest<TResponse>
-    {
-        public HttpGetRpcRequest(DaemonRpc daemonRpc, string endpoint) : base(endpoint, daemonRpc.HttpClient, daemonRpc.HttpRpcRequestOptions)
-        {
-        }
-    }
-}
--- TurtlecoinRpc/Request/Http/Daemon/HttpPostRpcRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TurtlecoinRpc.Request.Http.Daemon
-{
-    internal class HttpPostRpcRequest<TResponse, TRequest> : Http.HttpPostRpcRequest<TResponse, TRequest>
-    {
-        public HttpPostRpcRequest(DaemonRpc daemonRpc, string endpoint, TRequest request) : base(endpoint, request, daemonRpc.HttpClient, daemonRpc.HttpRpcRequestOptions)
-        {
-        }
-    }
-}
